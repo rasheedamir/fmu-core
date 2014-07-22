d522616121f14eef688fe9471682e5b2771bb485
Rasheed <rasheed.amir@live.com>
2014-07-22 10:19:11 +0200

    Further refined the configurations. And added database creation scripts in dev config.

---
+++ src/main/java/se/inera/fmu/core/config/Constants.java
@@ -0,0 +1,13 @@
+package se.inera.fmu.core.config;
+
+/**
+ * Created by Rasheed on 7/21/14.
+ *
+ * Application Constants
+ *
+ */
+public interface Constants {
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+}
--- src/main/java/se/inera/fmu/core/config/DevFmuCoreConfiguration.java
+++ src/main/java/se/inera/fmu/core/config/DevFmuCoreConfiguration.java
@@ -2,6 +2,7 @@ package se.inera.fmu.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -16,11 +17,16 @@ public class DevFmuCoreConfiguration extends FmuCoreConfigurationSupport {
 
     @Bean
     @Override
+    @Profile(Constants.SPRING_PROFILE_DEVELOPMENT)
     public DataSource dataSource() {
         // Embedded H2 Database
         // Read Configurations from property file
-        EmbeddedDatabase dataSource = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+        EmbeddedDatabase dataSource = new EmbeddedDatabaseBuilder()
+                                            .setType(EmbeddedDatabaseType.H2)
+                                            .addScript("classpath:org/activiti/db/create/activiti.h2.create.engine.sql")
+                                            .addScript("classpath:org/activiti/db/create/activiti.h2.create.history.sql")
+                                            .addScript("classpath:org/activiti/db/create/activiti.h2.create.identity.sql")
+                                            .build();
         return dataSource;
     }
 }
-
